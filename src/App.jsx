96b117e35109c1d6257df58b590813cf2be13726
Greg Kurka <gregkurka@gmail.com>
2025-01-31 12:20:01 -0600

    Followed workshop sprcifications.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Link, Routes, Route } from "react-router-dom";
+import Red from "./components/Red/Red";
+import Blue from "./components/Blue/Blue";
+import Home from "./components/Home/Home";
+
 function App() {
-  return <>App</>;
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
